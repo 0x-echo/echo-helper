d784b1eb9b5b3033515594043683d5c67b85caca
Airyland <i@mao.li>
2022-10-30 19:16:57 +0800

    show toolbar on whitelisted domain

--- src/entry/toolbar.js
+++ src/entry/toolbar.js
@@ -1,10 +1,15 @@
 import { createApp } from 'vue'
 import EchoWidget from '../view/EchoWidget'
 
-const app = createApp(EchoWidget)
 
-const div = document.createElement('div')
-div.id = 'chrome_echo-widget'
-document.body.appendChild(div)
+// whitelist
+const url = document.location.href
+if (/^https:\/\/opensea\.io/.test(url)) {
+  const app = createApp(EchoWidget)
 
-app.mount('#chrome_echo-widget')
+  const div = document.createElement('div')
+  div.id = 'chrome_echo-widget'
+  document.body.appendChild(div)
+  
+  app.mount('#chrome_echo-widget')
+}
